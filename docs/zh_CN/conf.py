a95e510487bba086efd9f87cd2f2ce370d51cc90
YangYuqi317 <YangYuqi317@gmail>
2022-11-18 10:59:54 +0800

    commit message

--- docs/zh_CN/conf.py
+++ docs/zh_CN/conf.py
@@ -22,10 +22,7 @@ extensions = [
     'recommonmark'
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
+
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
